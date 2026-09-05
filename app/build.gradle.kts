939812711ca830c5b8f7581a26783ed8072be6d5
maria0010101 <maria0010101@users.noreply.github.com>
2026-09-05 17:21:00 +0800

    feat(v0.3): 更新應用程式全新 3D 顯示圖示並升級版本至 0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,9 @@ android {
         applicationId = "com.example.myapplication"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.2"
+        versionCode = 3
+        versionName = "0.3"
+
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

3698ec903e96a8291e8a5f33ddd5aa09afb8f65f
maria0010101 <maria0010101@users.noreply.github.com>
2026-09-13 17:29:09 +0800

    feat(v0.4): 更新說明文件並升級版本至 0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.example.myapplication"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.3"
+        versionCode = 4
+        versionName = "0.4"
 
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
